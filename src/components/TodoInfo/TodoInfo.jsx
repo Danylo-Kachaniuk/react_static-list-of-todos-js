84c95757a1b5206c2e1033f6cc302e3cb2a90876
Danylo Kachaniuk <logistik2004@ukr.net>
2025-05-06 20:51:22 +0300

    add solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,37 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    <UserInfo user={todo.user} />
+  </article>
+);
+
+/*
+<article className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">CSS</h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
+
+    <article className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">JS</h2>
+
+      <a className="UserInfo" href="mailto:Shanna@melissa.tv">
+        Ervin Howell
+      </a>
+    </article>
+
+    <article className="TodoInfo">
+      <h2 className="TodoInfo__title">React</h2>
+
+      <a className="UserInfo" href="mailto:Nathan@yesenia.net">
+        Clementine Bauch
+      </a>
+    </article>
+*/
